a73226c76fd18d3d493a818ffbaabb1afdc66c00
srivastava-socialmesh <srivastava.g@live.com>
2026-03-18 22:40:47 +0530

    Update middleware.ts

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -1,6 +1,5 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { type NextRequest, NextResponse } from 'next/server'
-import { Database } from '@/types/supabase'
 
 export const createClient = (request: NextRequest) => {
   // Create an unmodified response
